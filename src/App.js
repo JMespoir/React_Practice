3803a3319a39938922bc335d78c39d3480e8d3d2
JMEspoir <maro3534@knu.ac.kr>
2023-03-31 22:36:17 +0900

    GuGuDan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, Fragment,useRef } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App=()=>{
+  const [num1,setNum1] = useState(Math.floor(Math.random()*9)+1);
+  const [num2,setNum2] = useState(Math.floor(Math.random()*9)+1);
+  const [value,setValue] = useState("");
+  const [cor,setCor] = useState(true);
+  const Input = useRef()
+  const [A,setA] = useState("");
+  const [visiable,setVisiable]=useState(false);
+
+
+  const OnChange=(e)=>{
+    setValue(e.target.value)
+  }
+  const OnSubmit=(e)=>{
+    setVisiable(true);
+    e.preventDefault();
+    setA(num1*num2);
+
+    if(num1*num2 === parseInt(value)){
+      setValue("");
+      setCor(true);
+    }
+    else{
+      setValue("");
+      setCor(false);
+    }
+    setNum1(Math.floor(Math.random()*9)+1);
+    setNum2(Math.floor(Math.random()*9)+1);
+    Input.current.focus();
+  }
+  
+  return(
+    <Fragment>
+      <span>{`${num1} * ${num2} 는?`}</span>
+      <form onSubmit={OnSubmit}>
+        <input ref={Input} onChange={OnChange} value={value}></input>
+        <button type="submit" >Submit!!</button>
+      </form>
+      {visiable ? <div>{cor ?`정답!! (${A})`: `오답!! (정답은 : (${A}))`}</div> : null}
+      
+    </Fragment>
   );
 }
-
 export default App;
